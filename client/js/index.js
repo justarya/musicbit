4274d7de7ac2314738f7574204f1f06e3e237e28
justarya <just1arya@gmail.com>
2019-08-09 14:05:46 +0700

    Task Views development 2

--- client/js/index.js
+++ client/js/index.js
@@ -1,4 +1,4 @@
-$(function() {
+$('document').ready(function(){
     $('#spotify-form').submit(function(event) {
       event.preventDefault();
       $.get('http://localhost:3000/authorize' + $.param(), function(data) {
@@ -6,4 +6,59 @@ $(function() {
       })
 
   });
-})()
+  initial();
+  console.log('init')
+})
+function initial(){
+  if(isSignIn()){
+    $('#main').hide();
+    if(isSignInSpotify()){
+      $('#music-stats').show();
+      $('#spotify--login').hide();
+    }else{
+      $('#music-stats').hide();
+      $('#spotify--login').show();
+    }
+  }else{
+    $('#spotify--login').hide();
+    $('#main').show();
+    $('#music-stats').hide();
+  }
+}
+function isSignInSpotify(){
+  if(localStorage.getItem('spotify_token')){
+    return true;
+  }else{
+    return false;
+  }
+}
+function isSignIn(){
+  if(localStorage.getItem('token')){
+    return true
+  }else{
+    return false;
+  }
+}
+function onSignIn(googleUser) {
+  let id_token = googleUser.getAuthResponse().id_token;
+  $.ajax({
+    method: 'post',
+    url: 'http://localhost:3000/user/login',
+    data: {
+      id_token
+    }
+  })
+  .done((data) => {
+    localStorage.setItem('token',data)
+  })
+  .fail((jqXHR,err) => {
+    console.error(jqXHR);
+  })
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}

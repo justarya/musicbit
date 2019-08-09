1bd885e739189a6bb538c22c7cac673f017380f4
justarya <just1arya@gmail.com>
2019-08-09 14:04:10 +0700

    Task Views development

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const controllers = require('../controllers/');
 
-router.get('/login',controllers.user.login);
+router.post('/login',controllers.user.login);
 
 module.exports = router;
